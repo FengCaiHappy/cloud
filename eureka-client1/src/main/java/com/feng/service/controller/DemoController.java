a0c32382a9275d5a2acdb5efa597130ff00827f8
tanghh <297107653@qq.com>
2019-07-03 15:11:51 +0800

    1

--- eureka-client1/src/main/java/com/feng/service/controller/DemoController.java
+++ eureka-client1/src/main/java/com/feng/service/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.feng.service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class DemoController {
 
+    @Value("${config.value}")
+    private String value;
+
     @RequestMapping("/demo")
     @ResponseBody
     public String demo(){
-        return "demo";
+        return value;
     }
 }
