9a9f51d402fa9b729b45cd065918c4db89ce4a3e
tanghh <297107653@qq.com>
2019-07-04 15:54:19 +0800

    去掉无用注解

--- eureka-server/src/main/java/com/feng/server/CloudApplication.java
+++ eureka-server/src/main/java/com/feng/server/CloudApplication.java
@@ -3,10 +3,8 @@ package com.feng.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 @EnableEurekaServer
 public class CloudApplication {
 
